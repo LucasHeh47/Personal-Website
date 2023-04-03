3dc22bf07a378c187a3cf229b4fd78b4328a06d0
LucasJ05 <darthmonkey062005@gmail.com>
2023-04-03 15:07:20 -0700

    Contact Form Spam Prevention
    Added reCAPTCHA v2 Checkbox

--- Personal Website/sendmail.php
+++ Personal Website/sendmail.php
@@ -3,6 +3,19 @@ ini_set("display_errors", 1);
 ini_set("display_startup_errors", 1);
 error_reporting(E_ALL);
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $recaptcha_secret_key = "6LcrMVclAAAAAMKBnDjNRCUScM24zFWitiVWmTzw";
+    $recaptcha_response = $_POST["g-recaptcha-response"];
+    $remote_ip = $_SERVER["REMOTE_ADDR"];
+
+    $url = "https://www.google.com/recaptcha/api/siteverify?secret=" . $recaptcha_secret_key . "&response=" . $recaptcha_response . "&remoteip=" . $remote_ip;
+
+    $recaptcha_verification_response = file_get_contents($url);
+    $recaptcha_verification_result = json_decode($recaptcha_verification_response);
+
+    if (!$recaptcha_verification_result->success) {
+        echo "recaptcha_error";
+        exit;
+    }
     $name = $_POST["name"];
     $email = $_POST["email"];
     $message = $_POST["message"];
